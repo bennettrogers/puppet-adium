52872db5b0efb00d7b746fc8d069bef6bee03b00
Bennett Rogers <bennett@crocodoc.com>
2013-08-29 00:09:11 -0700

    first try at adding plugins

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,4 +8,10 @@ class adium {
     provider => 'appdmg',
     source   => 'http://download.adium.im/Adium_1.5.6.dmg',
   }
+
+  # Install plugins, if any
+  file { "/Users/${::boxen_user}/Library/Application Support/Adium 2.0":
+    source  => "puppet:///adium/plugins",
+    recurse => true,
+  }
 }
